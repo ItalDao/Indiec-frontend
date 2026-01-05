2542cadd37b782d03287e95841abb588d7901f4b
ItalDao <mtsalcivar@gmail.com>
2026-01-05 00:00:12 -0500

    refactor: reorganize imports and update routing structure in AppRouter

--- src/apps/shell/router/AppRouter.tsx
+++ src/apps/shell/router/AppRouter.tsx
@@ -1,40 +1,39 @@
-// src/apps/shell/router/AppRouter.tsx
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import AdminLayout from '../layout/AdminLayout';
-import ClientLayout from '../layout/ClientLayout';
-import HomePage from '../../client/home/presentation/pages/HomePage';
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 
-//import UsuariosPage from '../../../admin/presentation/pages/UsuariosPage';
-//import RolesPage from '../../../admin/presentation/pages/RolesPage';
-//import { NuevaVista } from '../../client/events/presentation/pages/nuevaVista';
+// Admin Pages
+import AdminLayout from '../layout/AdminLayout';
+import GeneralSettingsPage from "../../admin/settings/presentation/pages/general/GeneralSettingsPage";
+import StaticPageForm from "../../admin/settings/presentation/pages/static-pages/StaticPageForm";
+import StaticPagesList from "../../admin/settings/presentation/pages/static-pages/StaticPagesList";
+import UsersList from "../../admin/settings/presentation/pages/users/UsersList";
+import UserForm from "../../admin/settings/presentation/pages/users/UserForm";
+import CatalogsList from "../../admin/settings/presentation/pages/catalogs/CatalogsList";
+import RolesList from "../../admin/settings/presentation/pages/roles/RolesList";
 import { EventsPage } from '../../client/events';
 
+// Client Pages
+import ClientLayout from '../layout/ClientLayout';
+import HomePage from '../../client/home/presentation/pages/HomePage';
 import { ArtistListPage } from '../../client/artists/presentation/pages/ArtistListPage';
 import { ArtistDetailPage } from '../../client/artists/presentation/pages/ArtistDetailPage';
-import { MisGustos } from '../../client/artists/presentation/components/MisGustos';
-
-/* 👉 IMPORTS DE TU MÓDULO STORE (CLIENT) */
-import { StorePage } from '../../../apps/client/store/presentation/pages/StorePage';
-import { ProductDetailPage } from '../../../apps/client/store/presentation/pages/ProductDetailPage';
-import { CartPage } from '../../client/account/presentation/pages/CartPage/CartPage';
-import { CheckoutPage } from '../../client/account/presentation/pages/CheckoutPage/CheckoutPage';
-import { ProfilePage } from '../../client/account/presentation/pages/ProfilePage/ProfilePage';
-import { OrderDetailPage } from '../../client/account/presentation/pages/OrderDetailPage/OrderDetailPage';
-import { FavoritesPage } from '../../client/account/presentation/pages/FavoritesPage/FavoritesPage';
-import { OrdersPage } from '../../client/account/presentation/pages/OrdersPage/OrdersPage';
-
-//  IMPORTAR LOS COMPONENTES REALES
 import { SongListPage } from '../../client/songs/presentation/pages/SongListPage';
 import { SongDetailPage } from '../../client/songs/presentation/pages/SongDetailPage';
-// src/apps/shell/router/AppRouter.tsx
+import { StorePage } from '../../client/store/presentation/pages/StorePage';
+import { ProductDetailPage } from '../../client/store/presentation/pages/ProductDetailPage';
+import { CartPage } from '../../client/account/presentation/pages/CartPage/CartPage';
+import { CheckoutPage } from '../../client/account/presentation/pages/CheckoutPage/CheckoutPage';
+import { OrdersPage } from '../../client/account/presentation/pages/OrdersPage/OrdersPage';
+import { OrderDetailPage } from '../../client/account/presentation/pages/OrderDetailPage/OrderDetailPage';
+import { FavoritesPage } from '../../client/account/presentation/pages/FavoritesPage/FavoritesPage';
+import { ProfilePage } from '../../client/account/presentation/pages/ProfilePage/ProfilePage';
 import { AboutPage } from '../../client/static/presentation/pages/AboutPage';
 import { TermsPage } from '../../client/static/presentation/pages/TermsPage';
 import { PrivacyPage } from '../../client/static/presentation/pages/PrivacyPage';
 import { ContactPage } from '../../client/static/presentation/pages/ContactPage';
 import { FAQPage } from '../../client/support/presentation/pages/FAQPage';
 import { UserSettingsPage } from '../../client/settings/presentation/pages/UserSettingsPage';
-
 import { PreferencesPage } from '../../client/preferences/pages/PreferencesPage';
+import { MisGustos } from '../../client/artists/presentation/components/MisGustos';
 
 const TempPage = ({ title }: { title: string }) => (
   <div style={{ padding: '2rem', textAlign: 'center' }}>
@@ -52,7 +51,7 @@ export const AppRouter = () => {
         {/* REDIRECCIÓN INICIAL */}
         <Route path="/" element={<Navigate to="/client/home" replace />} />
 
-        {/* ================= ADMIN ================= */}
+        {/* ===== ADMIN ===== */}
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<Navigate to="/admin/dashboard" replace />} />
           <Route path="dashboard" element={<TempPage title="Dashboard" />} />
@@ -75,24 +74,15 @@ export const AppRouter = () => {
           </Route>
         </Route>
 
-        {/* ================= CLIENT ================= */}
+        {/* ===== CLIENT ===== */}
         <Route path="/client" element={<ClientLayout />}>
           <Route index element={<Navigate to="/client/home" replace />} />
           <Route path="home" element={<HomePage />} />
           <Route path="search" element={<TempPage title="Búsqueda" />} />
-          <Route path="artists" element={<TempPage title="Artistas" />} />
-          <Route path="artists/:id" element={<TempPage title="Detalle de Artista" />} />
-          <Route path="about" element={<AboutPage />} />
-<Route path="terms" element={<TermsPage />} />
-<Route path="privacy" element={<PrivacyPage />} />
-<Route path="contact" element={<ContactPage />} />
-          <Route path="faq" element={<FAQPage />} />
-          <Route path="settings" element={<UserSettingsPage />} />
-          <Route path="preferences" element={<PreferencesPage />} />
 
+          <Route path="artists" element={<ArtistListPage />} />
+          <Route path="artists/:id" element={<ArtistDetailPage />} />
 
-
-          {/*  RUTAS REALES */}
           <Route path="songs" element={<SongListPage />} />
           <Route path="songs/:id" element={<SongDetailPage />} />
           
@@ -104,17 +94,28 @@ export const AppRouter = () => {
           <Route path="cart" element={<CartPage />} />
           <Route path="checkout" element={<CheckoutPage />} />
 
-          {/* ✅ NUEVA RUTA (LISTADO DE PEDIDOS) */}
+          {/* ======== PEDIDOS ======== */}
           <Route path="orders" element={<OrdersPage />} />
-
-          {/* 📄 DETALLE DE PEDIDO */}
           <Route path="orders/:id" element={<OrderDetailPage />} />
 
+          {/* ======== CUENTA ======== */}
           <Route path="favorites" element={<FavoritesPage />} />
           <Route path="profile" element={<ProfilePage />} />
+          <Route path="settings" element={<UserSettingsPage />} />
+          <Route path="preferences" element={<PreferencesPage />} />
+
+          {/* ======== PÁGINAS ESTÁTICAS ======== */}
+          <Route path="about" element={<AboutPage />} />
+          <Route path="terms" element={<TermsPage />} />
+          <Route path="privacy" element={<PrivacyPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="faq" element={<FAQPage />} />
         </Route>
 
-        {/* ================= 404 ================= */}
+        {/* RUTA SUELTA */}
+        <Route path="/masterclass" element={<MisGustos />} />
+
+        {/* 404 */}
         <Route path="*" element={<TempPage title="404 - Página no encontrada" />} />
       </Routes>
     </BrowserRouter>
